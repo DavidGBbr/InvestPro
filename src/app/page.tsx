0184e8fdd3878ec3fbc680e008b7f012761929aa
davidGBbr <davidgomesbrigido@gmail.com>
2023-10-06 22:45:57 -0300

    filtered list created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import { items } from "@/data/items";
+import { filterListByMonth, getCurrentMonth } from "@/helpers/dateFilter";
+import { Item } from "@/types/Item";
+import React, { useState, useEffect } from "react";
 
 const Page = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
   return (
     <div>
       <div id="header" className="bg-blue-800 h-36 text-center">
